35d45d3694dad8c32c96d6d1c3c79691598441a2
Sirius <sirius-sama@proton.me>
2022-10-11 21:24:31 +0600

    cookies path

--- cr.py
+++ cr.py
@@ -13,6 +13,7 @@ from pymediainfo import MediaInfo
 # Global variables
 current_directory = os.getcwd()
 output_directory = current_directory
+cookies = f"{current_directory}/src/cookies/cr-cookies.txt"
 config = config.config
 
 
@@ -31,7 +32,7 @@ meta = vars(args)
 
 # Checking crunchyroll version
 if 'beta' in meta['URL']:
-    info_json_cmd = f"yt-dlp --external-downloader aria2c {meta['URL']} -f 'best[height={meta['resolution']}]' --no-progress --write-info-json --no-download --cookies '{current_directory}/src/cookies/cr-cookies.txt' -o '{current_directory}/episode.%(ext)s'"
+    info_json_cmd = f"yt-dlp --external-downloader aria2c {meta['URL']} -f 'best[height={meta['resolution']}]' --no-progress --write-info-json --no-download --cookies '{cookies}' -o '{current_directory}/episode.%(ext)s'"
 else:
     info_json_cmd = f"yt-dlp --external-downloader aria2c {meta['URL']} -f 'best[height={meta['resolution']}]' --no-progress --write-info-json --no-download -o '{current_directory}/episode.%(ext)s'"
 
@@ -99,7 +100,7 @@ properTitle = properTitle.replace(' ', '.').replace('..','.')
 if 'beta' in meta['URL']:
     download_cmd = f"yt-dlp --external-downloader aria2c {meta['URL']} -f 'best[height={meta['episode_info']['height']}]' --write-subs -o '{current_directory}/[RAW]{properTitle}.%(ext)s'"
 else:
-    download_cmd = f"yt-dlp --external-downloader aria2c {meta['URL']} -f 'best[height={meta['episode_info']['height']}]' --write-subs --cookies '{current_directory}/src/cookies/cr-cookies.txt' -o '{current_directory}/[RAW]{properTitle}.%(ext)s'"
+    download_cmd = f"yt-dlp --external-downloader aria2c {meta['URL']} -f 'best[height={meta['episode_info']['height']}]' --write-subs --cookies '{cookies}' -o '{current_directory}/[RAW]{properTitle}.%(ext)s'"
 
 
 print('Downloading raw video and subtitles...')
