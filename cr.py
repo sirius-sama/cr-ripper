639c54bc5376fdc1fa15b018bb9b85feb111d259
Sirius <sirius-sama@proton.me>
2022-10-13 04:25:25 +0600

    added new args and other stuff

--- cr.py
+++ cr.py
@@ -2,13 +2,13 @@
 
 import os
 import json
+import glob
 import time
 import argparse
 import requests
 import subprocess
 from data import config
 from pymediainfo import MediaInfo
-
 
 # Global variables
 current_directory = os.getcwd()
@@ -22,16 +22,17 @@ parser = argparse.ArgumentParser(description='This script rips content from crun
 
 parser.add_argument('URL', type=str, metavar='URL...', help='URL of the episode')
 parser.add_argument('-res', '--resolution', type=str, metavar='', required=False, nargs='?', default='1080', help='Choose the resolution of the episode', choices=['240', '360', '480', '720', '1080'])
-parser.add_argument('-title', '--title', type=str, metavar='', required=False, nargs='?', default='y', help='Use this when you want to use episode name in the main title')
-parser.add_argument('-tag', '--tag', type=str, metavar='', required=False, nargs='?', help='Name of the release group')
+parser.add_argument('-title', '--title', action='store_true', required=False, help='Use episode name in title')
+parser.add_argument('-pro', '--pro', action='store_true', required=False, help='Premium episode')
+parser.add_argument('-tag', '--tag', type=str, metavar='', required=False, nargs='?', default=str(config['tag']), help='Name of the release group')
 
 
 args = parser.parse_args()
 
 meta = vars(args)
 
-# Checking crunchyroll version
-if 'beta' in meta['URL']:
+# Checking crunchyroll version (free of premium)
+if args.pro == True:
     info_json_cmd = f"yt-dlp --external-downloader aria2c {meta['URL']} --cookies '{cookies}' -f 'best[height={meta['resolution']}]' --no-progress --write-info-json --no-download --cookies '{cookies}' -o '{current_directory}/episode.%(ext)s'"
 else:
     info_json_cmd = f"yt-dlp --external-downloader aria2c {meta['URL']} -f 'best[height={meta['resolution']}]' --no-progress --write-info-json --no-download -o '{current_directory}/episode.%(ext)s'"
@@ -70,34 +71,21 @@ else:
 
 
 
-
-if meta['tag'] != 'null':
-    tag = meta['tag']
-
-elif meta['tag'] == 'null' and config['tag'] != '':
-    tag = config['tag']
-
-else:
-    tag = 'NOGROUP'
-
-
-
-
-if meta['title'] != 'y':
-    ep_name = ''
-else:
+if args.title == True:
     ep_name = meta['episode_info']['episode']
-    ep_name = ep_name.replace('!','').replace('&','and').replace('~','').replace('!!','')
+    ep_name = ep_name.replace('&','and').replace('~','')
+else:
+    ep_name = ''
 
 
 
-properTitle = f"{meta['episode_info']['series']}.{season}{episode}.{ep_name}.{meta['episode_info']['height']}.CR.WEB-DL.AAC2.0.H.264-{tag}"
+properTitle = f"{meta['episode_info']['series']}.{season}{episode}.{ep_name}.{meta['episode_info']['height']}.CR.WEB-DL.AAC2.0.H.264-{args.tag}"
 properTitle = properTitle.replace(' ', '.').replace('..','.')
 
 
 
-# Checking crunchyroll version
-if 'beta' in meta['URL']:
+# Checking crunchyroll version (free of premium)
+if args.pro == True:
     download_cmd = f"yt-dlp --external-downloader aria2c {meta['URL']} --cookies '{cookies}' -f 'best[height={meta['episode_info']['height']}]' --write-subs --sub-langs 'en.*' -o '{current_directory}/[RAW]{properTitle}.%(ext)s'"
 else:
     download_cmd = f"yt-dlp --external-downloader aria2c {meta['URL']} -f 'best[height={meta['episode_info']['height']}]' --write-subs --sub-langs 'en.*' -o '{current_directory}/[RAW]{properTitle}.%(ext)s'"
@@ -111,9 +99,17 @@ info_json.wait()
 print('Download has been finished.')
 
 
+# Getting subtitle file location
+result = [f for f in glob.glob('*US.ass') if f'{properTitle}']
+
+for f in result:
+    subtitle_file = os.path.abspath(f)
+
+print(subtitle_file)
+
 
 # Muxing
-muxing_cmd = f"ffmpeg -i '[RAW]{properTitle}.mp4' -i '[RAW]{properTitle}.en-US.ass' -vcodec copy -acodec copy -map 0 -map 1 -metadata:s:a:0 language=jpn -metadata:s:s:0 language=eng -metadata:s:s:0 title=English -disposition:s:0 default -c copy {properTitle}.mkv"
+muxing_cmd = f"ffmpeg -i '[RAW]{properTitle}.mp4' -i '{subtitle_file}' -vcodec copy -acodec copy -map 0 -map 1 -metadata:s:a:0 language=jpn -metadata:s:s:0 language=eng -metadata:s:s:0 title=English -disposition:s:0 default -c copy {properTitle}.mkv"
 
 muxing = subprocess.Popen(muxing_cmd, shell=True, text=True)
 
