90fdf0e52e41388560c50fe6209610ceaa966023
Sirius <sirius-sama@proton.me>
2022-10-13 21:41:50 +0600

    testingPatch

--- cr.py
+++ cr.py
@@ -134,7 +134,7 @@ shutil.move(f"{current_directory}/meta.json", f"{folder_path}/meta.json")
 # Cleaning up RAW video and subtitle
 # raw_files = glob.glob('[RAW]*')
 
-raw_files = [f for f in glob.glob(f".*") if "[RAW]" in f]
+raw_files = [f for f in glob.glob(f"*{properTitle}*") if "RAW" in f]
 
 for raw_file in raw_files:
     cli_ui.info(cli_ui.red, "Removing", cli_ui.red, cli_ui.bold, f"{raw_file}")
