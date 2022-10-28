3d7b8a3239b4e0630ac4ca9638227b5882588341
Sirius <sirius-sama@proton.me>
2022-10-28 07:37:16 +0600

    -

--- cr.py
+++ cr.py
@@ -163,6 +163,9 @@ def downloadAndMux(URL):
 
         elif meta['episode_info']['episode_number'] < 10:
             meta['episode'] = f"E{str(meta['episode_info']['episode_number']).zfill(2)}"
+        
+        elif meta['episode_info']['episode_number'] > 100:
+            meta['episode'] = f"E{meta['episode_info']['episode_number']}"
 
         # else:
         #     episode = f"E{meta['episode_info']['episode_number']}"
